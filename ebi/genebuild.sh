ad8969e51bd9b7999272c8a17b6b5dabd38b16d1
William Stark <williamstarkbio@gmail.com>
2021-03-30 19:12:41 +0100

    don't load genebuild.sh; move selected settings to .bashrc_ebi

--- ebi/genebuild.sh
+++ ebi/genebuild.sh
@@ -5,7 +5,7 @@ export GB_HOME=/nfs/production/panda/ensembl/genebuild
 # basic.sh
 ################################################################################
 # if [[ -z "$ENSEMBL_SOFTWARE_HOME" ]] && [[ -e "/nfs/software/ensembl/latest/envs/basic.sh" ]]; then
-#   source /nfs/software/ensembl/latest/envs/basic.sh
+#     source /nfs/software/ensembl/latest/envs/basic.sh
 # fi
 # ws
 # use my version of basic.sh
@@ -16,27 +16,27 @@ ENSEMBL_SOFTWARE_HOME=/nfs/software/ensembl/RHEL7-JUL2017-core2
 
 # Homebrew (Linuxbrew)
 if [[ -f /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/linuxbrew.sh ]]; then
-  source /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/linuxbrew.sh
+    source /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/linuxbrew.sh
 fi
 
 # plenv
 # if [[ -f /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/plenv.sh ]]; then
-#   source /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/plenv.sh
+#     source /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/plenv.sh
 # fi
 
 # pyenv
 # if [[ -f /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/pyenv.sh ]]; then
-#   source /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/pyenv.sh
+#     source /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/pyenv.sh
 # fi
 
 # MySQL commands
 # https://www.ebi.ac.uk/seqdb/confluence/display/ENS/MySQL+commands
 # if [[ -f /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/mysql-cmds.sh ]]; then
-#   source /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/mysql-cmds.sh
+#     source /nfs/software/ensembl/RHEL7-JUL2017-core2/envs/mysql-cmds.sh
 # fi
 mysql_cmd_dir=/nfs/software/ensembl/mysql-cmds
 if [[ -d $mysql_cmd_dir ]]; then
-    export PATH="${mysql_cmd_dir}/ensembl/bin:${mysql_cmd_dir}/ensemblgenomes/bin:$PATH"
+    export PATH="${mysql_cmd_dir}/ensembl/bin:$PATH"
 fi
 ################################################################################
 
@@ -57,7 +57,7 @@ if [[ -n "$LINUXBREW_HOME" ]];then
 fi
 
 if [[ -d "/nfs/production/panda/ensembl/production/ensemblftp/data_files" ]];then
-  export FTP_DIR="/nfs/production/panda/ensembl/production/ensemblftp/data_files"
+    export FTP_DIR="/nfs/production/panda/ensembl/production/ensemblftp/data_files"
 fi
 
 export HIVE_EMAIL="$USER@ebi.ac.uk"
@@ -79,7 +79,7 @@ shopt -s direxpand
 #alias mkgbdir="mkdir -m 2775"
 
 reload_ensembl_release() {
-    EVERSION=$(mysql-ens-meta-prod-1 ensembl_metadata -NB -e "SELECT ensembl_version FROM data_release WHERE is_current = 1;")
+    EVERSION=$(mysql-ens-meta-prod-1 ensembl_metadata --skip-column-names --execute="SELECT ensembl_version FROM data_release WHERE is_current = 1;")
     if [[ $EVERSION -gt ${ENSEMBL_RELEASE:-0} ]]; then
         export ENSEMBL_RELEASE=$EVERSION
     elif [[ $EVERSION -lt $ENSEMBL_RELEASE ]];then
