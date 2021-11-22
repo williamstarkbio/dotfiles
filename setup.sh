ee38697947e798d00aac9cbb315614d9a589ead2
William Stark <williamstarkbio@gmail.com>
2021-11-22 00:36:55 +0000

    update package information after adding source

--- setup.sh
+++ setup.sh
@@ -403,6 +403,7 @@ install_desktop_packages() {
 install_google_chrome() {
     wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt update
     sudo apt install -y google-chrome-stable
 }
 
