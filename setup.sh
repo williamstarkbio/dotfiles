2974e2992bc321805af9e4a36466f5ac228ee061
William Stark <williamstarkbio@gmail.com>
2023-05-06 11:51:34 +0100

    docs

--- setup.sh
+++ setup.sh
@@ -360,6 +360,7 @@ setup_neovim() {
 
     # setup vim-plug
     # https://github.com/junegunn/vim-plug
+    # "$HOME/.local/share/nvim/site/autoload/plug.vim"
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
     # install packages with vim-plug
